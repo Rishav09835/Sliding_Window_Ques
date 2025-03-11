13dc4ad472eb31a63c8f5bd9891e42ca4a21a4a4
Rishav Dobriyal <73049840+Rishav09835@users.noreply.github.com>
2025-03-11 13:21:53 +0530

    updated the code for LSWRC

--- 001_LSWRC/LSWRC.cpp
+++ 001_LSWRC/LSWRC.cpp
@@ -8,24 +8,30 @@ int LSWRC(string s){
     int maxl = 0;
     int i = 0, j=0;
     int l=0;
-    unordered_set<char>st;
+    vector<int>freq(256,0);
     while(j<n){
-        if(st.count(s[j])>0){
-            //shrink window
-            while(s[i]!=s[j]){
-                st.erase(s[i]);
-                i++;
-            }
-            st.erase(s[i]);
+        freq[s[j]]++;
+        // if(st.count(s[j])){
+        //     //shrink window
+        //     while(s[i]!=s[j]){
+        //         st.erase(s[i]);
+        //         i++;
+        //     }
+        //     st.erase(s[i]);
+        //     i++;
+        // }
+        // else{
+        //     st.insert(s[j]);
+        //     l=j-i+1;
+        //     maxl = max(maxl,l);
+        //     j++;
+        // }
+        while(freq[s[j]]>1){
+            freq[s[i]]--;
             i++;
         }
-        else{
-            st.insert(s[j]);
-            l=j-i+1;
-            maxl = max(maxl,l);
-            j++;
-        }
-
+        maxl = max(maxl, j-i+1);
+        j++;
     }
     return maxl;
 }
